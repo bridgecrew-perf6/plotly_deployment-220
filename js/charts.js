0c363930342ee43abe7268a9ba2f7c9fc580daf4
John Salvador <jsal9300@Johns-MacBook-Pro.local>
2020-12-17 22:00:06 -0800

    updated files

--- js/charts.js
+++ js/charts.js
@@ -23,6 +23,7 @@ function init() {
 // Initialize the dashboard
 init();
 
+
 function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   buildMetadata(newSample);
